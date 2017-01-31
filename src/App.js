9754af3a7516880c2186506682e3acdad79b7775
khamilatul <khamilatul.zahro999@gmail.com>
2017-01-31 13:44:25 +0700

    create react

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FilterableProductTable from './FilterableProductTable';
 
-class App extends Component {
+ class App extends Component {
   render() {
     return (
       <div className="App">
@@ -13,6 +14,10 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+        Hello World
+        </p>
+        <FilterableProductTable/>
       </div>
     );
   }
